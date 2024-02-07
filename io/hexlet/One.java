536c4484bb34cbd598e08165e99e76dbc657e50c
Iliatar <a.u.tarasov@gmail.com>
2024-02-07 22:04:28 +0300

    import StringUtils

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,10 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
 	public static void main(String[] args){
-		Two.greeting();
+		String reversedWord = StringUtils.reverse("hexlet");
+		System.out.println(reversedWord);
 	}
 }
